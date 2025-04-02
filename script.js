3c4db378c57744cd98b8151c858c474b02ebb4ed
Adrian Gidaro <adriangidaro@gmail.com>
2025-04-02 20:35:50 +1100

    Finished JS intro

--- script.js
+++ script.js
@@ -46,3 +46,58 @@ console.log(Boolean([]))
 console.log(typeof('Hi'))
 console.log(typeof(12))
 console.log(typeof(2.5))
+
+console.log(1/2)
+console.log(1/"2") // Type coercion
+console.log(1/0)
+console.log(1/"hi")
+
+// Explicit cast
+x = "2"
+console.log(1/Number(x))
+
+// Array (list in Python)
+const names = ['John', 'Jane', 'Bob'] // Declares array as constant, not the contents
+const foo = ['Matt', 52, 184.5]
+console.log(names[1])
+// names[4] = 'Mary'
+names.push('Mary', 'Jason')
+// names.pop() // Removes last element
+console.log(names)
+const name = names.pop()
+console.log(name)
+const bigArray = names.concat(foo)
+console.log(bigArray)
+
+// Objects - dict in Python
+const person1 = {
+    // Python - "name": "John"
+    name: "John",
+    age: 32,
+    city: "Sydney"
+}
+console.log(person1)
+console.log(person1['age'])
+console.log(person1.age)
+person1.country = "Australia"
+console.log(person1)
+Object.freeze(person1)
+person1.city = "Melbourne"
+console.log(person1)
+
+const person2 = {
+    name: "Sally",
+    age: 45,
+    address: {  // Sub-object
+        city: "Melbourne",
+        country: "Australia"
+    }
+}
+console.log(person2)
+console.log(person2.address.city)
+
+// const attr = prompt('Which attribute?')
+// console.log(person2.address[attr])
+
+// Optional chaining with ?
+console.log(person1.address?.country) // ? Makes address truthy - will only continue if defined, returns undefined and avoids returning an error
